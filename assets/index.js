3f47763a33a773c3b7d0b6b9efd395d5e708f90b
Raavi Kaur <raavi211299@gmail.com>
2024-11-09 16:39:25 -0600

    add code in js file to show alert on button click

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+numberOfDrums = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrums; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked");
+  });
+}
