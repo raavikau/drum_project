547c8b18cb486b827c9bda31059cff5f3c33103c
Raavi Kaur <raavi211299@gmail.com>
2024-11-11 17:04:52 -0600

    optimize the index file code

--- assets/index.js
+++ assets/index.js
@@ -1,51 +1,38 @@
 numberOfDrums = document.querySelectorAll(".drum").length;
 
+var sounds = {
+  w: "assets/sounds/tom-1.mp3",
+  a: "assets/sounds/tom-2.mp3",
+  s: "assets/sounds/tom-3.mp3",
+  d: "assets/sounds/tom-4.mp3",
+  j: "assets/sounds/snare.mp3",
+  k: "assets/sounds/crash.mp3",
+  l: "assets/sounds/kick-bass.mp3",
+};
+
 for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonPress = this.innerHTML;
-    makeSound(buttonPress);
-    buttonAnimation(buttonPress);
+    handleInteraction(buttonPress);
   });
 }
 
 document.addEventListener("keydown", function (event) {
-  makeSound(event.key);
-  buttonAnimation(event.key);
+  handleInteraction(event.key);
 });
 
-function makeSound(key) {
-  switch (key) {
-    case "w":
-      var tom1 = new Audio("assets/sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "a":
-      var tom2 = new Audio("assets/sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      var tom3 = new Audio("assets/sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      var tom4 = new Audio("assets/sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      var snare = new Audio("assets/sounds/snare.mp3");
-      snare.play();
-      break;
-    case "k":
-      var crash = new Audio("assets/sounds/crash.mp3");
-      crash.play();
-      break;
-    case "l":
-      var kick = new Audio("assets/sounds/kick-bass.mp3");
-      kick.play();
-      break;
-    default:
-      console.log(buttonPress);
+function handleInteraction(key) {
+  if (sounds[key]) {
+    makeSound(key);
+    buttonAnimation(key);
+  } else {
+    console.log(key);
   }
+}
+
+function makeSound(key) {
+  var audio = new Audio(sounds[key]);
+  audio.play();
 }
 
 function buttonAnimation(currentKey) {
