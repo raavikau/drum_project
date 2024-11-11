c0a81ad24f80898c823f03a66cdb0ded8791c2c8
Raavi Kaur <raavi211299@gmail.com>
2024-11-11 17:15:29 -0600

    add the comment to the index file

--- assets/index.js
+++ assets/index.js
@@ -10,21 +10,23 @@ var sounds = {
   l: "assets/sounds/kick-bass.mp3",
 };
 
+// Add click event listener to each drum button
 for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var buttonPress = this.innerHTML;
+    var buttonPress = this.innerHTML; // to identify which button is press
     handleInteraction(buttonPress);
   });
 }
 
+// add event listener to detect keyboard press
 document.addEventListener("keydown", function (event) {
-  handleInteraction(event.key);
+  handleInteraction(event.key);  // pass the key value to the function
 });
 
 function handleInteraction(key) {
   if (sounds[key]) {
-    makeSound(key);
-    buttonAnimation(key);
+    makeSound(key);  // play sound to the associated key
+    buttonAnimation(key); // add animation effect to the button
   } else {
     console.log(key);
   }
@@ -37,8 +39,8 @@ function makeSound(key) {
 
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);
-  activeButton.classList.add("pressed");
+  activeButton.classList.add("pressed"); // add the class to apply animation
   setTimeout(function () {
     activeButton.classList.remove("pressed");
-  }, 100);
+  }, 100); // remove after 100ms to revert animation
 }
